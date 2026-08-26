03727363982f3c7516adfb459855e402d0ea3a25
adiban17 <adityabanerjeethechamp@gmail.com>
2026-08-27 01:00:06 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readline()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+        return requirements
+
+
+
+setup(
+    name = 'synthaset',
+    version = '0.1.0',
+    author = 'Aditya Banerjee',
+    author_email = 'adityabanerjee171@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
