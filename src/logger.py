180d047b7119d58cdda43e576a6ee7270a052c12
adiban17 <adityabanerjeethechamp@gmail.com>
2026-08-27 01:15:38 +0530

    added logger.py and exception.py

---
+++ src/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime("%d_%m%_Y_%H_%M_%S")}.log"
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+
+# Test 
+if __name__ == "__main__":
+    logging.info('Test INFO Message')
+    logging.warning('Test WARNING Message')
+    logging.error('Test ERROR Message')
+
+    print('Tests Completed :)')
